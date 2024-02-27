596f5d0d34aa0e30b6e5a5c766a2a7ff23891f04
sen.ting <sen.ting@mediatek.com>
2024-02-27 10:45:26 +0800

    [ALPS08664693] WeChat Message List Scroll lagging issue
    [Detail]
    ARM ticket: 03455996
    ARM patch: https://code.ipdelivery.arm.com/c/GPU/mali-ddk/+/6257
    
    GPUCORE-41012 Submit performance optimization for heavily fragmented scenario.
    
    Mediatek reported that their customer saw a performance issue with WeChat app stuttening after heavy use.
    Their analysis indicated the system was busy performing memory reclamation and compaction.
    This patch is reported to fix the issue however it cannot be verified internally.
    To reproduce the issue, the user need to run the Monkey app for over 1 hour to fragment the system memory before running WeChat application.
    
    MTK-Commit-Id: e97122974d0a254b821a4e9732eb7c3871fab470
    
    CR-Id: ALPS08664693
    Feature: ARM GPU DDK driver
    Change-Id: I19c5c57f60a2db0a048621e12fb44aa7326ebaa3

--- gpu/gpu_mali/mali_avalon/mali-r44p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_kcpu.c
+++ gpu/gpu_mali/mali_avalon/mali-r44p1/drivers/gpu/arm/midgard/csf/mali_kbase_csf_kcpu.c
@@ -2393,7 +2393,7 @@ static int delete_queue(struct kbase_context *kctx, u32 id)
 		destroy_workqueue(queue->cmds_timeout_wq);
 #endif /* CONFIG_MALI_MTK_FENCE_DEBUG */
 
-		kfree(queue);
+		vfree(queue);
 	} else {
 		dev_dbg(kctx->kbdev->dev,
 			"Attempt to delete a non-existent KCPU queue");
@@ -3222,7 +3222,7 @@ int kbase_csf_kcpu_queue_new(struct kbase_context *kctx, struct kbase_ioctl_kcpu
 		goto out;
 	}
 
-	queue = kzalloc(sizeof(*queue), GFP_KERNEL);
+	queue = vzalloc(sizeof(*queue));
 
 	if (!queue) {
 #if IS_ENABLED(CONFIG_MALI_MTK_CREATE_KCPU_QUEUE_DEBUG)
@@ -3257,7 +3257,7 @@ int kbase_csf_kcpu_queue_new(struct kbase_context *kctx, struct kbase_ioctl_kcpu
 			"%s: Fail to allocate workqueue", __func__);
 #endif /* CONFIG_MALI_MTK_LOG_BUFFER */
 #endif
-		kfree(queue);
+		vfree(queue);
 		ret = -ENOMEM;
 
 		goto out;
@@ -3280,7 +3280,7 @@ int kbase_csf_kcpu_queue_new(struct kbase_context *kctx, struct kbase_ioctl_kcpu
 #endif /* CONFIG_MALI_MTK_LOG_BUFFER */
 #endif
 			destroy_workqueue(queue->wq);
-			kfree(queue);
+			vfree(queue);
 			ret = -ENOMEM;
 			goto out;
 		}
@@ -3316,7 +3316,7 @@ int kbase_csf_kcpu_queue_new(struct kbase_context *kctx, struct kbase_ioctl_kcpu
 			"%s: Fail to allocate workqueue for cmds timeout", __func__);
 #endif /* CONFIG_MALI_MTK_LOG_BUFFER */
 #endif
-		kfree(queue);
+		vfree(queue);
 		ret = -ENOMEM;
 
 		goto out;
