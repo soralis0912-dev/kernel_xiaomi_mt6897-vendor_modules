a3eedbc19ed34fd970f395b0b870c81b4d6acd4e
Daniel Tao <daniel.tao@mediatek.com>
2024-03-20 22:18:38 +0800

    [ALPS08818896] GPU: GPUCORE-41441 for CVE-2024-1395
    [Description]
    The support for the fine grained GPU cache maintenance using the
    FLUSH_PA_RANGE command, after GPU page table update, introduced the
    possbility of 2 race scenarios when the PGD page is freed after the
    count of valid entries become 0 on unmap of GPU memory.
    
    This commit updates the MMU teardown code to prevent the possibility
    of 2 aforementioned race scenarios.
    
    [Patch]
    (c6438/2) GPUCORE-41441: Fix fine grained GPU cache maintenance code
    
    [ARM Ticket]
    03463062
    
    [Test]
    CTS, Stress test
    
    MTK-Commit-Id: c14391b562ff7fca8644306fe58db268ff0daa60
    
    Signed-off-by: Daniel Tao <daniel.tao@mediatek.com>
    CR-Id: ALPS08818896
    Feature: ARM GPU DDK driver
    Change-Id: I16a13c07d3d581cd61f52109b48c9680744ce382
    (cherry picked from commit 96cf31f42adc183bc3293a1519ebded163545460)

--- gpu/gpu_mali/mali_avalon/mali-r44p1/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu.c
+++ gpu/gpu_mali/mali_avalon/mali-r44p1/drivers/gpu/arm/midgard/mmu/mali_kbase_mmu.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note
 /*
  *
- * (C) COPYRIGHT 2010-2023 ARM Limited. All rights reserved.
+ * (C) COPYRIGHT 2010-2024 ARM Limited. All rights reserved.
  *
  * This program is free software and is provided to you under the terms of the
  * GNU General Public License version 2 as published by the Free Software
@@ -1217,8 +1217,12 @@ page_fault_retry:
 							     &op_param);
 			mmu_hw_operation_end(kbdev);
 		} else {
-			/* Can safely skip the invalidate for all levels in case
-			 * of duplicate page faults.
+			/* Usually it is safe to skip the MMU cache invalidate for all levels
+			 * in case of duplicate page faults. But for the pathological scenario
+			 * where the faulty VA gets mapped by the time page fault worker runs it
+			 * becomes imperative to invalidate MMU cache for all levels, otherwise
+			 * there is a possibility of repeated page faults on GPUs which supports
+			 * fine grained MMU cache invalidation.
 			 */
 			op_param.flush_skip_levels = 0xF;
 			op_param.vpfn = fault_pfn;
@@ -1266,8 +1270,12 @@ page_fault_retry:
 							     &op_param);
 			mmu_hw_operation_end(kbdev);
 		} else {
-			/* Can safely skip the invalidate for all levels in case
-			 * of duplicate page faults.
+			/* Usually it is safe to skip the MMU cache invalidate for all levels
+			 * in case of duplicate page faults. But for the pathological scenario
+			 * where the faulty VA gets mapped by the time page fault worker runs it
+			 * becomes imperative to invalidate MMU cache for all levels, otherwise
+			 * there is a possibility of repeated page faults on GPUs which supports
+			 * fine grained MMU cache invalidation.
 			 */
 			op_param.flush_skip_levels = 0xF;
 			op_param.vpfn = fault_pfn;
@@ -1769,6 +1777,12 @@ static void mmu_insert_pages_failure_recovery(struct kbase_device *kbdev,
 
 		if (!num_of_valid_entries) {
 			kbase_kunmap(p, page);
+
+			/* No CPU and GPU cache maintenance is done here as caller would do the
+			 * complete flush of GPU cache and invalidation of TLB before the PGD
+			 * page is freed. CPU cache flush would be done when the PGD page is
+			 * returned to the memory pool.
+			 */
 
 			kbase_mmu_add_to_free_pgds_list(mmut, p);
 
@@ -2757,13 +2771,21 @@ static void kbase_mmu_update_and_free_parent_pgds(struct kbase_device *kbdev,
 		    current_level != MIDGARD_MMU_LEVEL(0)) {
 			kbase_kunmap(p, current_page);
 
-			/* Ensure the cacheline containing the last valid entry
-			 * of PGD is invalidated from the GPU cache, before the
-			 * PGD page is freed.
-			 */
-			kbase_mmu_sync_pgd_gpu(kbdev, mmut->kctx,
-				current_pgd + (index * sizeof(u64)),
-				sizeof(u64), flush_op);
+			/* Check if fine grained GPU cache maintenance is being used */
+			if (flush_op == KBASE_MMU_OP_FLUSH_PT) {
+				/* Ensure the invalidated PTE is visible in memory right away */
+				kbase_mmu_sync_pgd_cpu(kbdev,
+						       kbase_dma_addr(p) + (index * sizeof(u64)),
+						       sizeof(u64));
+				/* Invalidate the GPU cache for the whole PGD page and not just for
+				 * the cacheline containing the invalidated PTE, as the PGD page is
+				 * going to be freed. There is an extremely remote possibility that
+				 * other cachelines (containing all invalid PTEs) of PGD page are
+				 * also present in the GPU cache.
+				 */
+				kbase_mmu_sync_pgd_gpu(kbdev, mmut->kctx, current_pgd,
+						       512 * sizeof(u64), KBASE_MMU_OP_FLUSH_PT);
+			}
 
 			kbase_mmu_add_to_free_pgds_list(mmut, p);
 		} else {
@@ -2946,13 +2968,21 @@ static int kbase_mmu_teardown_pgd_pages(struct kbase_device *kbdev, struct kbase
 		if (!num_of_valid_entries) {
 			kbase_kunmap(p, page);
 
-			/* Ensure the cacheline(s) containing the last valid entries
-			 * of PGD is invalidated from the GPU cache, before the
-			 * PGD page is freed.
-			 */
-			kbase_mmu_sync_pgd_gpu(kbdev, mmut->kctx,
-				pgd + (index * sizeof(u64)),
-				pcount * sizeof(u64), flush_op);
+			/* Check if fine grained GPU cache maintenance is being used */
+			if (flush_op == KBASE_MMU_OP_FLUSH_PT) {
+				/* Ensure the invalidated ATEs are visible in memory right away */
+				kbase_mmu_sync_pgd_cpu(kbdev,
+						       kbase_dma_addr(p) + (index * sizeof(u64)),
+						       pcount * sizeof(u64));
+				/* Invalidate the GPU cache for the whole PGD page and not just for
+				 * the cachelines containing the invalidated ATEs, as the PGD page
+				 * is going to be freed. There is an extremely remote possibility
+				 * that other cachelines (containing all invalid ATEs) of PGD page
+				 * are also present in the GPU cache.
+				 */
+				kbase_mmu_sync_pgd_gpu(kbdev, mmut->kctx, pgd, 512 * sizeof(u64),
+						       KBASE_MMU_OP_FLUSH_PT);
+			}
 
 			kbase_mmu_add_to_free_pgds_list(mmut, p);
 
